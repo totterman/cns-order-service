d5d48530453d6ab252bf2835aebc862c27d20c68
Petri Totterman <petri@totterman.com>
2023-11-08 15:36:23 +0000

    Monitoring

--- src/main/java/com/totterman/polarbookshop/orderservice/order/web/OrderController.java
+++ src/main/java/com/totterman/polarbookshop/orderservice/order/web/OrderController.java
@@ -3,6 +3,8 @@ package com.totterman.polarbookshop.orderservice.order.web;
 import com.totterman.polarbookshop.orderservice.order.domain.Order;
 import com.totterman.polarbookshop.orderservice.order.domain.OrderService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import reactor.core.publisher.Mono;
 @RequestMapping("orders")
 public class OrderController {
     private final OrderService orderService;
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
 
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
@@ -20,11 +23,13 @@ public class OrderController {
 
     @GetMapping
     public Flux<Order> getAllOrders(@AuthenticationPrincipal Jwt jwt) {
+        log.info("Fetching all orders");
         return orderService.getAllOrders(jwt.getSubject());
     }
 
     @PostMapping
     public Mono<Order> submitOrder(@RequestBody @Valid OrderRequest orderRequest) {
+        log.info("Order for {} copies of the book with ISBN {}", orderRequest.quantity(), orderRequest.isbn());
         return orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity());
     }
 }
